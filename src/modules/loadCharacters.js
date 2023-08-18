705840418a8c6b69643f8ba27b2c9cb36b69e12a
unknown <kipkemoiedgah@gmail.com>
2023-08-18 23:39:36 +0300

    Fixed eslint error

--- src/modules/loadCharacters.js
+++ src/modules/loadCharacters.js
@@ -2,16 +2,16 @@
 const myAppId = '9vrG0tyBXBwoGkanoEnf';
 
 // export const newCharacter = async () => {
-  // const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
-  // const appEndpoint = '/apps/';
+// const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+// const appEndpoint = '/apps/';
 
-  // Make a POST request to create a new app
-  // const createAppUrl = baseUrl + appEndpoint;
-  // const createAppResponse = await fetch(createAppUrl, { method: 'POST' });
-  // const appId = await createAppResponse.text();
+// Make a POST request to create a new app
+// const createAppUrl = baseUrl + appEndpoint;
+// const createAppResponse = await fetch(createAppUrl, { method: 'POST' });
+// const appId = await createAppResponse.text();
 
-  // Log the app ID
-  // console.log('App ID:', appId);
+// Log the app ID
+// console.log('App ID:', appId);
 // };
 
 export const fetchCharacters = async () => {
