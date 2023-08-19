c6564f051c9ca45694a41c97765f9420e59d9e7f
Muluken Zemed <zmmulu@gmail.com>
2023-08-19 20:12:55 +0300

    Revert "JavaScript Group Capstone project"

--- src/modules/countCharacters.js
+++
@@ -1,9 +0,0 @@
-import fetchCharacters from './fetchCharacters';
-
-const countCharacters = async () => {
-  const characters = await fetchCharacters();
-  const characterCountElement = document.getElementById('character-count');
-  characterCountElement.textContent = `Characters(${characters.length})`;
-};
-
-export default countCharacters;
--- src/modules/countComments.js
+++
@@ -1,5 +0,0 @@
-const countComments = async (data) => {
-  const commentCountElement = document.getElementById('comment-counter');
-  commentCountElement.textContent = `Comments(${data.length < 1 ? 0 : data.length})`;
-};
-export default countComments;
